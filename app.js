fb3db89d84e45405e05e3c013d9f47ee933a595c
David Ordovskiy <david.ordovsky@gmail.com>
2020-04-12 19:22:33 -0400

    finish up todo item js

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ function addTodo(e) {
   // don't refresh upon submit
   e.preventDefault(); 
 
+  /* this goes inside the ul
+  <div class="todo">
+    <li></li>
+    <button>complete</button>
+    <button>delete</button>
+  </div>
+  */
+
   // div for each entry 
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
@@ -29,4 +37,18 @@ function addTodo(e) {
 
   // item goes inside the div
   todoDiv.appendChild(newTodo);
+
+  // ------
+  // add the 'complete' button
+  const completeButton = document.createElement('button');
+  // button will have an icon from font-awesome
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+
+  // add the 'delete' button
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("complete-btn");
+  todoDiv.appendChild(deleteButton);
 }
